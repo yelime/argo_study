241690399c01619efdebbc0158ec86622e3f9a1e
yelime <yelime1226@naver.com>
2021-04-29 23:04:45 +0900

    2021/04/29 study

---
+++ baekjoon/bj_156/bj_15649.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class bj_15649 {
+    static boolean[] C;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.close();
+        C = new boolean[N];
+        fc(0, M, N, "");
+
+    }
+
+    public static void fc(int de, int MD, int MN, String result) {
+        if (de == MD - 1) {
+            for (int i = 0; i < MN; i++) {
+                if (!C[i])
+                    System.out.println(result + (i + 1));
+            }
+            return;
+        }
+
+        for (int i = 0; i < MN; i++) {
+            if (!C[i]) {
+                C[i] = true;
+                String Nresult = result + (i + 1) + " ";
+                fc(de + 1, MD, MN, Nresult);
+                C[i] = false;
+            }
+
+        }
+
+    }
+}
