17a0d3205ddc0afbfdf7d584d2027407ad063ee7
yelime <yelime1226@naver.com>
2021-05-14 23:59:48 +0900

    2021/05/14 study

---
+++ baekjoon/bj_101/bj_10171.java
@@ -0,0 +1,9 @@
+public class bj_10171 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
