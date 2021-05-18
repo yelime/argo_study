50afa0c97d1ef837ccf9260d3b240a2fbabfe442
yelime <yelime1226@naver.com>
2021-05-18 22:02:29 +0900

    2021/05/18 study

---
+++ baekjoon/bj_108/bj_10869.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bj_10869 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A,B;
+		A=sc.nextInt();
+		B=sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		sc.close();
+	}
+}
