6f23682df19caf0002cb5ed74a53785c8f7d05fc
yelime <yelime1226@naver.com>
2021-05-20 19:44:46 +0900

    2021/05/20 study

---
+++ baekjoon/bj_025/bj_02588.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bj_02588 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println(num1 * (num2%10));
+		System.out.println(num1 * ((num2%100)/10));
+		System.out.println(num1 * (num2/100));
+		System.out.println(num1 * num2);
+	}
+
+}
